07a03329682401706f64e57bb177a32b77f566da
Yulia Elagina <julia-elagina@yandex.ru>
2021-08-21 19:23:56 +0300

    Подключает Redux

--- project/src/index.js
+++ project/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {createStore} from 'redux';
-// import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
 
 import App from './components/app/app';
 import './styles/style.scss';
+import { reducer } from './store/reducer';
+
+const store = configureStore({
+  reducer: reducer,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
