ed0266abbefa4a95f26bf514efa2db7c9f612b15
Yulia Elagina <julia-elagina@yandex.ru>
2021-08-24 20:23:03 +0300

    Создает компонент формы заявки на кредит

---
+++ project/src/components/request-form/request-form.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import NumberFormat from 'react-number-format';
+
+import { CreditType } from '../../const';
+import { getCreditType, getInitialPayment, getPeriod, getRequestNumber, getTotalSum } from '../../store/selectors';
+import { formatMoneyString, formatRequestNumber, getPeriodLabel } from '../../utils';
+import './style.scss';
+
+const typeToTitle = {
+  [CreditType.AUTO]: 'Автокредит',
+  [CreditType.HOME]: 'Ипотека',
+};
+
+const typeToLabel = {
+  [CreditType.AUTO]: 'Стоимость автомобиля',
+  [CreditType.HOME]: 'Стоимость недвижимости',
+};
+
+function RequestForm() {
+  const requestNumber = useSelector(getRequestNumber);
+  const creditType = useSelector(getCreditType);
+  const totalPrice = useSelector(getTotalSum);
+  const initialPayment = useSelector(getInitialPayment);
+  const period = useSelector(getPeriod);
+
+  const fields = [
+    {
+      id: 'number',
+      text: 'Номер заявки',
+      value: formatRequestNumber(requestNumber),
+    },
+    {
+      id: 'purpose',
+      text: 'Цель кредита',
+      value: typeToTitle[creditType],
+    },
+    {
+      id: 'price',
+      text: typeToLabel[creditType],
+      value: formatMoneyString(totalPrice),
+    },
+    {
+      id: 'initial-payment',
+      text: 'Первоначальный взнос',
+      value: formatMoneyString(initialPayment),
+    },
+    {
+      id: 'period',
+      text: 'Срок кредитования',
+      value: `${period} ${getPeriodLabel(period)}`,
+    },
+  ];
+
+  return (
+    <section className="request-form">
+      <h3>Шаг 3. Оформление заявки</h3>
+      <form action="#" method="post" id="request-form">
+        {fields.map(({id, text, value}) => (
+          <div key={id} className="request-form__item">
+            <label htmlFor={`request-${id}`}>{text}</label>
+            <input type="text" id={`request-${id}`} name={`request-${id}`} value={value} readOnly tabIndex="-1"/>
+          </div>
+        ))}
+        <div className="request-form__wrapper">
+          <div className="request-form__field request-form__field--user">
+            <label htmlFor="request-user" className="visually-hidden">ФИО</label>
+            <input type="text" name="request-user" id="request-user" placeholder="ФИО" required/>
+          </div>
+          <div className="request-form__field request-form__field--contact">
+            <label htmlFor="request-phone" className="visually-hidden">Телефон</label>
+            <NumberFormat
+              id="request-phone"
+              name="request-phone"
+              format="+#(###) ###-##-##"
+              mask="_"
+              type="tel"
+              pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}"
+              placeholder="Телефон"
+              required
+            />
+          </div>
+          <div className="request-form__field request-form__field--contact">
+            <label htmlFor="request-email" className="visually-hidden">E-mail</label>
+            <input type="email" name="request-email" id="request-email" placeholder="E-mail" required/>
+          </div>
+          <button className="request-form__button button" type="submit">Отправить</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default RequestForm;
