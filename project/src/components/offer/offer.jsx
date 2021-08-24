e7dc76013b4c289f7ecb5cf7ea2e0367619268cd
Yulia Elagina <julia-elagina@yandex.ru>
2021-08-24 17:43:45 +0300

    Добавляет обработчик на кнопку в компоненте Предложение банка

--- project/src/components/offer/offer.jsx
+++ project/src/components/offer/offer.jsx
@@ -1,21 +1,35 @@
-import React, { Fragment } from 'react';
-import { useMemo } from 'react';
-import {useSelector} from 'react-redux';
+import React, { Fragment, useCallback } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+
 import { CreditType, MinLoanSum } from '../../const';
-import { getCreditType, getValidityStatus, selectIsValidLoanSum, selectLoanSum, selectMinIncome, selectMonthlyPayment, selectPercentRate } from '../../store/selectors';
+import { formatMoneyString } from '../../utils';
+import { setStep } from '../../store/actions';
+import { getCreditType, getFormStep, getValidityStatus, selectIsValidLoanSum, selectLoanSum, selectMinIncome, selectMonthlyPayment, selectPercentRate } from '../../store/selectors';
 import './style.scss';
+
+const typeToText = {
+  [CreditType.AUTO]: 'автокредита',
+  [CreditType.HOME]: 'ипотеки',
+};
+
+const typeToMessage = {
+  [CreditType.AUTO]: 'автокредиты',
+  [CreditType.HOME]: 'ипотечные кредиты',
+};
 
 function Offer() {
   const creditType = useSelector(getCreditType);
   const isValidPrice = useSelector(getValidityStatus);
   const isValidLoan = useSelector(selectIsValidLoanSum);
-  const loanSum = useSelector(selectLoanSum).toLocaleString('ru-RU');
+  const loanSum = useSelector(selectLoanSum);
   const percentRate = useSelector(selectPercentRate).toFixed(2).replace('.', ',');
-  const monthlyPayment = useSelector(selectMonthlyPayment).toLocaleString('ru-RU');
-  const monthlyIncome = useSelector(selectMinIncome).toLocaleString('ru-RU');
+  const monthlyPayment = useSelector(selectMonthlyPayment);
+  const monthlyIncome = useSelector(selectMinIncome);
+  const formStep = useSelector(getFormStep);
+  const isDisabled = formStep === 3;
+  const dispatch = useDispatch();
 
-  const creditText = useMemo(() => (creditType === CreditType.AUTO) ? 'автокредита' : 'ипотеки', [creditType]);
-  const messageText = useMemo(() => (creditType === CreditType.AUTO) ? 'автокредиты' : 'ипотечные кредиты', [creditType]);
+  const onButtonClick = useCallback(() => dispatch(setStep(3)), [dispatch]);
 
   const renderContent = () => {
     if (!isValidPrice) {
@@ -26,7 +40,7 @@ function Offer() {
     if (!isValidLoan) {
       return (
         <Fragment>
-          <h3>Наш банк не выдает {messageText} меньше {MinLoanSum[creditType].toLocaleString('ru-RU')} рублей.</h3>
+          <h3>Наш банк не выдает {typeToMessage[creditType]} меньше {MinLoanSum[creditType].toLocaleString('ru-RU')} рублей.</h3>
           <p>Попробуйте использовать другие параметры для расчета.</p>
         </Fragment>
       );
@@ -36,23 +50,29 @@ function Offer() {
         <h3>Наше предложение</h3>
         <dl className="offer__details">
           <div className="offer__item">
-            <dt>{loanSum} рублей</dt>
-            <dd>Сумма {creditText}</dd>
+            <dt>{formatMoneyString(loanSum)}</dt>
+            <dd>Сумма {typeToText[creditType]}</dd>
           </div>
           <div className="offer__item">
             <dt>{percentRate}%</dt>
             <dd>Процентная ставка</dd>
           </div>
           <div className="offer__item">
-            <dt>{monthlyPayment} рублей</dt>
+            <dt>{formatMoneyString(monthlyPayment)}</dt>
             <dd>Ежемесячный платеж</dd>
           </div>
           <div className="offer__item">
-            <dt>{monthlyIncome} рублей</dt>
+            <dt>{formatMoneyString(monthlyIncome)}</dt>
             <dd>Необходимый доход</dd>
           </div>
         </dl>
-        <button className="offer__button button">Оформить заявку</button>
+        <button
+          className="offer__button button"
+          type="button"
+          disabled={isDisabled}
+          onClick={onButtonClick}
+        >Оформить заявку
+        </button>
       </Fragment>
     );
   };
